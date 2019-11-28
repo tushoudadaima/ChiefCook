b5bbe422bb1bae8ac7712d3ee15b01834eddddbe
tushoudadaima <1790196608@qq.com>
2019-11-28 09:54:48 +0800

    主干

---
+++ shixun/app/src/main/java/com/example/administrator/shixun/ThreeFragment.java
@@ -0,0 +1,18 @@
+package com.example.administrator.shixun;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThreeFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.threefragment, container,false);
+
+        return view;
+    }
+}
